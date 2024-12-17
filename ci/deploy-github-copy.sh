6864ee1f8f95bc5eec26b2149bba219eb3c11a57
maximus3 <max2000.gym5cheb@yandex.ru>
2024-12-17 15:44:50 +0300

    fix

--- ci/deploy-github-copy.sh
+++ ci/deploy-github-copy.sh
@@ -6,7 +6,7 @@ echo "$SSH_KEY" | tr -d '\r' > ~/.ssh/deploy_key
 sudo chmod 600 ~/.ssh/deploy_key
 ssh -i ~/.ssh/deploy_key -p $SSH_PORT -o StrictHostKeyChecking=no $SSH_USERNAME@$SSH_HOST "echo Ping && mkdir -p $CODE_FOLDER && exit"
 tar -czf $CODE_FOLDER.tar.gz *
-scp -i ~/.ssh/deploy_key -p $SSH_PORT -o StrictHostKeyChecking=no $CODE_FOLDER.tar.gz $SSH_USERNAME@$SSH_HOST:~/$CODE_FOLDER
+scp -i ~/.ssh/deploy_key -P $SSH_PORT -o StrictHostKeyChecking=no $CODE_FOLDER.tar.gz $SSH_USERNAME@$SSH_HOST:~/$CODE_FOLDER
 ssh -i ~/.ssh/deploy_key -p $SSH_PORT -o StrictHostKeyChecking=no $SSH_USERNAME@$SSH_HOST "
   cd $CODE_FOLDER;
   tar -xzf $CODE_FOLDER.tar.gz
