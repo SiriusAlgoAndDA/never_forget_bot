263657de20072bd3d380724d7ed0cf1af150615b
maximus3 <max2000.gym5cheb@yandex.ru>
2024-12-17 15:29:00 +0300

    try fix deploy

---
+++ ci/deploy-github-copy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Environment: $CI_ENVIRONMENT_NAME"
+
+mkdir -p ~/.ssh
+echo "$SSH_KEY" | tr -d '\r' > ~/.ssh/deploy_key
+sudo chmod 600 ~/.ssh/deploy_key
+ssh -i ~/.ssh/deploy_key -p $SSH_PORT -o StrictHostKeyChecking=no $SSH_USERNAME@$SSH_HOST "echo Ping && mkdir -p $CODE_FOLDER && exit"
+tar -czf $CODE_FOLDER.tar.gz *
+scp -i ~/.ssh/deploy_key -p $SSH_PORT -o StrictHostKeyChecking=no $CODE_FOLDER.tar.gz $SSH_USERNAME@$SSH_HOST:~/$CODE_FOLDER
+ssh -i ~/.ssh/deploy_key -o StrictHostKeyChecking=no -p $SSH_PORT $SSH_USERNAME@$SSH_HOST "
+  cd $CODE_FOLDER;
+  tar -xzf $CODE_FOLDER.tar.gz
+  echo \"$ENV\" > .env;
+  make docker-stop postgres || echo \"Postgres not run\";
+  make update;
+  exit"
