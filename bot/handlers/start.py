322d37140be912e59fbe674f3a3d4072b9036194
kolya13245 <kolya.gnusov@gmail.com>
2024-12-19 12:33:22 +0300

    Add .temporal.env.example

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -13,6 +13,6 @@ router = aiogram.Router()
 async def handler_start(message: types.Message) -> None:
     send_timestamp = (datetime.datetime.now() + datetime.timedelta(seconds=10)).timestamp()
     if message.from_user is None:
-        raise RuntimeError("User is None")
+        raise RuntimeError('User is None')
     await new_event(message.from_user.id, 'Hello from Temporal!', send_timestamp)
     await message.reply('Hello!')
