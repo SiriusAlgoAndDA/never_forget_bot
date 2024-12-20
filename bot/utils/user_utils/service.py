4b575628bb138f2986febb3c78bbf19f058582fa
LeonidAndGame <leonidkorablin2@gmail.com>
2024-12-20 11:02:32 +0300

    new_promt

--- bot/utils/user_utils/service.py
+++ bot/utils/user_utils/service.py
@@ -5,6 +5,6 @@ from bot.database import models
 
 async def current_time(user: models.User) -> str:
     cur_time = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=user.timezone))).strftime(
-        '%H:%M:%S %d.%m.%Y'
+        '%H:%M:%S %d.%m.%Y %A'
     )
     return cur_time
