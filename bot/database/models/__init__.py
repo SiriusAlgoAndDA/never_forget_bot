9ce417060ee243e67e101112f65ccba4552b131e
kolya13245 <kolya.gnusov@gmail.com>
2024-12-20 09:49:13 +0300

    Add notification database

--- bot/database/models/__init__.py
+++ bot/database/models/__init__.py
@@ -1,10 +1,12 @@
 from .base import BaseModel
 from .event import Event
 from .user import User
+from .notification import Notification
 
 
 __all__ = [
     'BaseModel',
     'User',
     'Event',
+    'Notification'
 ]
--- bot/database/models/event.py
+++ bot/database/models/event.py
@@ -10,28 +10,42 @@ from .base import BaseModel
 class Event(BaseModel):
     __tablename__ = 'event'
 
-    type: orm.Mapped[str] = sa.Column(
-        'type',
-        sa.String,
-        nullable=False,
-        index=True,
-    )
     name: orm.Mapped[str] = sa.Column(
         'name',
         sa.String,
         nullable=False,
         index=False,
+        doc = "Само событие"
     )
+
+    status: orm.Mapped[str] = sa.Column(
+        'status',
+        sa.String,
+        nullable=False,
+        default='pending',
+        index=True,
+        doc = "Статус события: pending, completed..."
+    )
+
     time: orm.Mapped[datetime.datetime] = sa.Column(
         'time',
         sa.TIMESTAMP(timezone=True),
         nullable=False,
+        doc = "Время события datetime"
+    )
+
+    reschedule_timedelta: orm.Mapped[datetime.timedelta] = sa.Column(
+        'reschedule_timedelta',
+        sa.Interval,
+        nullable=True,
+        doc = "Интервал для пересчёта (timedelta)"
     )
 
     user_id: orm.Mapped[uuid.UUID] = sa.Column(  # type: ignore
-        'user_id',
+        'user_id',  
         sa.UUID(as_uuid=True),
         sa.ForeignKey('user.id'),
         nullable=False,
         index=True,
+        doc = "Идентификатор пользователя (UUID)"
     )
